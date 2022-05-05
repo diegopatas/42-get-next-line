90b26bcce4dc707dcc6cdc1479f8d20cb419d6de
ddiniz <ddiniz@student.42sp.org.br>
2022-05-05 20:47:22 -0300

    Gnl header: add definition and prototype

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 22:32:15 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/05/04 22:36:20 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/05/05 20:30:53 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NETX_LINE_H
 #define GET_NEXT_LINE_H
 
-#define BUFFER_SIZE
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 256
+# endif
 
 char	*get_next_line(int fd);
 
