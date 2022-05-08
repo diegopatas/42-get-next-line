991626854f6bb459b0aa2d41b8db6588f129681b
ddiniz <ddiniz@student.42sp.org.br>
2022-05-07 23:23:25 -0300

    GNL: improve the logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,19 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 21:42:25 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/05/06 20:05:29 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/05/07 23:23:07 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include "get_next_line.h"
+
+void	ft_verify_eol(char ch)
+{
+	if (ch == '\n' || ch == '\0')
+		return (NULL);
+}
 
 static size_t	ft_strlen(const char *str)
 {
@@ -41,58 +47,68 @@ static void	ft_strlcpy(const char *dest, const char *src, const size_t size)
 
 char	*ft_strjoin(const char *dest, const char *src)
 {
-	char	*joint;
-	char	*joint_first_p;
+	char	*join;
+	char	*join_start;
 	size_t	total_size;
 
 	total_size = ft_strlen(dest) + ft_strlen(src) + 1;
-	joint = (char *)malloc(total_size * sizeof(char));
-	joint_first_p = joint;
-	if (!joint)
+	join = (char *)malloc(total_size * sizeof(char));
+	join_start = join;
+	if (!join)
 		return (0);
 	while (*dest)
 	{
-		*joint = *dest;
+		*join = *dest;
 		dest++;
-		joint++;
+		join++;
 	}
 	while (*src)
 	{
-		*joint = *src;
+		*join = *src;
 		src++;
-		joint++;
+		join++;
 	}
-	*joint = '\0';
-	return (joint_first_id);
+	*join = '\0';
+	return (join_start);
 }
+
 
 char	*get_next_line(int fd)
 {
+	// VAR DECLARATION
 	static char		buff[BUFFER_SIZE];
 	static size_t	id_scan;
 	static size_t	id_start;
-	char			*str_res;
-	char			*str_aux;
+	static char		*str_line;
+	static char		*str_aux;
 
-	str_res = (char *)malloc(1);
-	str_res = "";
-	str_aux = str_res;
-	if (id_scan >= BUFFER_SIZE - 1)
+	//VERIFY SCANNING
+	if (id_scan >= BUFFER_SIZE - 1 || buff == NULL)
 	{
 		read(fd, buff, BUFFER_SIZE);
 		id_scan = 0;
 	}
-	if (buff[id_scan] == '\0');
-		return ("");
-	if (buff[id_scan] != '\n' && id_scan < BUFFER_SIZE)
+
+	// VERIFY END OF LINE OR FILE
+	ft_verify_eol(buff[id_scan]);
+
+	//BUILD LINE
+	id_start = id_scan;
+	while (id_scan < BUFFER_SIZE)
 	{
-		id_start = id_scan;
 		while (buff[id_scan] != '\n' && buff[id_scan] != '\0')
+		{
+			str_aux = (char *)malloc((id_scan - id_start + 1) * sizeof(char));
+			ft_strlcpy(str_aux, buff[id_start], (id_scan - id_start + 1));
+			str_line = ft_strjoin(str_line, str_aux);
+			free(str_aux);
+			str_aux = NULL;
 			id_scan++;
-		str_aux = (char *)malloc((id_scan - id_start + 1) * sizeof(char));
-		ft_strlcpy(str_aux, buff[id_start], (id_scan - id_start + 1));
-		str_res = ft_strjoin(str_res, str_aux);
-		free(str_aux);
+		}
+		read(fd, buff, BUFFER_SIZE);
+		id_scan = 0;
 	}
-	return (str_res);
+
+	//RETURN THE LINE
+	return (str_line);
 }
